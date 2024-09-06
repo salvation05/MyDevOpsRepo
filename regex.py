ad3e121f89c459861315a0f2384f73d20960cc62
salvation05 <piyushscorpio05@gmail.com>
2024-09-06 21:33:58 +0530

    new files

--- regex.py
+++ regex.py
@@ -6,10 +6,3 @@ if re.match(pattern,"helllo"):
     print("match1")
 if re.match(pattern,"ello"):
     print("match2")    
-
-#Metacharacters (^ and $)  
-pattern2=r"^piyush$"
-if re.match(pattern2,"helllo"):
-    print("match1")
-if re.match(pattern2,"ello"):
-    print("match2")
