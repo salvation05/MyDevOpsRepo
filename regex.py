987a7c52db958d7838709e15c0d881ddcd15c538
salvation05 <piyushscorpio05@gmail.com>
2024-09-06 20:17:20 +0530

    deleted regex.py

--- regex.py
+++
@@ -1,8 +0,0 @@
-#Metacharacters (.) DOT matches any character except new line
-
-import re
-pattern=r"hel.o"
-if re.match(pattern,"helllo"):
-    print("match1")
-if re.match(pattern,"ello"):
-    print("match2")    
